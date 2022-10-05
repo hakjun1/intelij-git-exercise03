75c0ee9ec534e9dbc0ef4a0caa6d8920f25b452c
Hakjun <hiatus9014@gmail.com>
2022-10-05 10:31:28 +0900

    first commit

---
+++ src/Calculator.java
@@ -0,0 +1,18 @@
+public class Calculator {
+   int a=0,b=0;
+    public void plus(int a,int b){
+        System.out.println(a+b);
+    }
+    public void minus(int a,int b){
+        System.out.println(a-b);
+    }
+    public void multiple(int a,int b){
+        System.out.println(a*b);
+    }
+    public void divide(int a,int b){
+        float result = a/b;
+        System.out.println(result);
+    }
+
+
+}
---
+++ src/CalculatorMain.java
@@ -0,0 +1,10 @@
+public class CalculatorMain {
+    public static void main(String[] args) {
+        Calculator aa = new Calculator();
+
+        aa.plus(10,20);
+        aa.minus(10,20);
+        aa.multiple(10,20);
+        aa.divide(10,20);
+    }
+}
