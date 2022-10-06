104c594c03f5e8c0c44aa611b703eb8def55029b
Hakjun <hiatus9014@gmail.com>
2022-10-06 14:22:41 +0900

    알파벳 숫자 세기

---
+++ src/Ex1008/ReadFile.java
@@ -0,0 +1,23 @@
+package Ex1008;
+
+import java.io.FileReader;
+import java.io.IOException;
+import java.io.Reader;
+
+//실습6 한바이트 읽기
+//char readOneByte(String filename)
+//지정한 파일의 맨 앞 한byte를 읽어오는 method를 만들어 보세요
+public class ReadFile {
+
+    public static void main(String[] args) throws IOException {
+        Reader reader = new FileReader("C:\\SelfStudyJava\\java_exercise_main\\testtxt");
+        while(true){
+            int data = reader.read();
+            if(data == -1){
+                break;
+            }
+            System.out.println((char)data);
+        }
+        reader.close();
+    }
+}
